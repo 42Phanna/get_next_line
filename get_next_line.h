e1ff55e1fcadd086ad08d5cdcc81512777a71c4a
Philippe ANNA <phanna@e3r2p23.42.fr>
2017-05-19 05:14:15 +0200

    first_comit

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/19 03:56:51 by phanna            #+#    #+#             */
+/*   Updated: 2017/05/19 04:52:29 by phanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+int	get_next_line(const int fd, char **line);
+#endif
