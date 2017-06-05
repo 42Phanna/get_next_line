84b8fc61f16dfb5b043a54f309d78af6c9bea622
Philippe ANNA <phanna@e3r13p2.42.fr>
2017-06-05 07:19:40 +0200

    h

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: phanna <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/19 04:14:32 by phanna            #+#    #+#             */
-/*   Updated: 2017/06/05 05:47:39 by phanna           ###   ########.fr       */
+/*   Updated: 2017/06/05 07:19:29 by phanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ int	ft_check_box(char *box, char **line)
 	while (box[i] && box[i] != '\n')
 		++i;
 	if (box[i] == '\n')
+	{
 		*line = ft_strsub(box, 0, i);
-	while (box[++i])
-		rest[++j] = box[i];
-	rest[j] = '\0';
-	ft_strcpy(box, rest);
+		while (box[++i])
+			rest[++j] = box[i];
+		rest[j] = '\0';
+		ft_strcpy(box, rest);
+	}
 	if (*line)
 		return (1);
 	return (0);
@@ -47,7 +49,7 @@ int	get_next_line(const int fd, char **line)
 	if (!box)
 		box = ft_memalloc(1);
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
-	{
+	{S  
 		buff[ret] = '\0';
 		box = ft_strjoin(box, buff);
 		if (ft_check_box(box, line) == 1)
@@ -55,6 +57,8 @@ int	get_next_line(const int fd, char **line)
 			ft_putstr(box);
 			return (1);
 		}
+		ft_putstr(box);
+		ft_putstr("\nNEXT\n");
 	}
 	return (0);
 }
